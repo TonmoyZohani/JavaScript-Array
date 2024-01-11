ac3021d72a7b73cfee09960e039c129120d48124
tonmoy-zohani <tonmoyzohani@gmail.com>
2024-01-11 23:52:08 +0600

    objects reference

---
+++ objects.js
@@ -0,0 +1,4 @@
+// let obj1 = { key: "value" };
+// let obj2 = obj1;
+// obj2.key = "new value";
+// console.log(obj1.key); // obj1.key will also be "new value" since both variables reference the same object.
