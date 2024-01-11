5661a9ef5ea1f2abc500e654e4f2f4d9b2819ed3
tonmoy-zohani <tonmoyzohani@gmail.com>
2024-01-11 23:55:05 +0600

    enumerating object properties

--- objects.js
+++ objects.js
@@ -2,3 +2,12 @@
 // let obj2 = obj1;
 // obj2.key = "new value";
 // console.log(obj1.key); // obj1.key will also be "new value" since both variables reference the same object.
+
+const persons = {
+  name: "Tonmoy",
+  id: 123456,
+};
+
+for (let property in persons) {
+  console.log(property);
+}
